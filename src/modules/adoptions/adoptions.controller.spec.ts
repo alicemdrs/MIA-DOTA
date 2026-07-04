19edaf4473a32f94d4f3ce210369cd8db796b1a4
Alice <medeirosmalice25@gmail.com>
2026-07-04 15:41:47 -0300

    criacao dos modulos

---
+++ src/modules/adoptions/adoptions.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AdoptionsController } from './adoptions.controller';
+
+describe('AdoptionsController', () => {
+  let controller: AdoptionsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AdoptionsController],
+    }).compile();
+
+    controller = module.get<AdoptionsController>(AdoptionsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});

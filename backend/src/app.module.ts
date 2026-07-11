18539cfb0de178ab8ad039262eefce23e4cbca07
Alice <medeirosmalice25@gmail.com>
2026-07-11 15:43:18 -0300

    define escopo do projeto

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AnimalsModule } from './modules/animals/animals.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { AdoptionsModule } from './modules/adoptions/adoptions.module';
+import { VisitsModule } from './modules/visits/visits.module';
+import { UploadsModule } from './modules/uploads/uploads.module';
+import { GeolocationModule } from './modules/geolocation/geolocation.module';
+import { HealthModule } from './modules/health/health.module';
+
+@Module({
+  imports: [AnimalsModule, OrganizationsModule, AdoptionsModule, VisitsModule, UploadsModule, GeolocationModule, HealthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

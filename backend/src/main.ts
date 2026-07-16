05e5cf2444d8952bb2fdcdd4d78e57f43d117b46
Alice <medeirosmalice25@gmail.com>
2026-07-16 12:08:48 -0300

    deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,6 @@ const corsOrigins = (process.env.CORS_ORIGIN ?? defaultCorsOrigins)
 app.enableCors({
     origin: '*', // Permite que qualquer origem (como o seu frontend local) acesse a API
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
 
   app.useGlobalPipes(

e8e24574b14775bff62f80cc549d17fa6cbc2d56
JanyeliceD <janyelice.d@escolar.ifrn.edu.br>
2026-07-15 21:44:08 -0300

    test: adiciona rota debug

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('debug-status')
+getDebug() {
+  return { status: 'backend rodando e atualizado!' };
+}
 }
